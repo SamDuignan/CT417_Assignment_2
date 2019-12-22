4a7dbe70da620fa6eeb748ff07a6b903f277efe7
SamDuignan <57842143+SamDuignan@users.noreply.github.com>
2019-12-22 19:07:04 +0000

    J unit first suc build

---
+++ src/main/java/ie/nuig/ct417_Assignment_2/Course.java
@@ -0,0 +1,53 @@
+package SamDuignan.app;
+
+import java.util.ArrayList;
+
+import org.joda.time.*;
+
+public class Course {
+
+	private ArrayList<Module> modules = new ArrayList<Module>();
+	private DateTime start;
+	private DateTime end;
+    private String name;
+
+	public Course(String course, ArrayList<Module> module, DateTime start, DateTime end) {
+		this.name = course;
+		this.modules = module;
+		this.start = start;
+		this.end = end;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String course) {
+		this.name = course;
+	}
+
+	public ArrayList<Module> getModules() {
+		return this.modules;
+	}
+
+	public void setModules(ArrayList<Module> modules) {
+		this.modules = modules;
+	}
+
+	public DateTime getStart() {
+		return this.start;
+	}
+
+	public DateTime getEnd() {
+		return this.end;
+	}
+
+	public void setStart(DateTime start) {
+		this.start = start;
+	}
+
+	public void setEnd(DateTime end) {
+		this.end = end;
+	}
+	
+}
---
+++ src/test/java/ie/nuig/ct417_Assignment_2/AppTest.java
@@ -0,0 +1,58 @@
+package SamDuignan.app;
+
+import java.util.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.joda.time.*;
+
+public class AppTest {
+
+    /**
+     * Test of getId method, of class Student.
+     */
+    @org.junit.Test
+    public void testGetId() {
+        System.out.println("getId");
+        Student instance = new Student("Jamie Dunne", 18, new DateTime("2018-05-05T10:11:12.123"), 1234);
+        int expResult = 1234;
+        int result = instance.getID();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getName method, of class Student.
+     */
+    @org.junit.Test
+    public void testGetName() {
+        System.out.println("getName");
+        Student instance = new Student("Jamie Dunne", 18, new DateTime("2018-05-05T10:11:12.123"), 1234);
+        String expResult = "Jamie Dunne";
+        String result = instance.getName();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getDob method, of class Student.
+     */
+    @org.junit.Test
+    public void testGetDob() {
+        System.out.println("getDob");
+        DateTime expResult = new DateTime("2018-05-05T10:11:12.123");
+        Student instance = new Student("Jamie Dunne", 18, expResult, 1234);
+        DateTime result = instance.getDob();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getUser method, of class Student.
+     */
+    @org.junit.Test
+    public void testGetUser() {
+        System.out.println("getUser");
+        Student instance = new Student("Jamie Dunne", 18, new DateTime("2018-05-05T10:11:12.123"), 1234);
+        String expResult = "Jamie Dunne18";
+        String result = instance.getUsername();
+        assertEquals(expResult, result);
+    }
+    
+}
